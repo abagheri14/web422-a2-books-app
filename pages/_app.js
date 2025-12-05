4470009a03fedd3f6e25b9c6c002b2c3d79bc9dd
Ali Bagheri <155804715+abagheri14@users.noreply.github.com>
2025-12-05 18:02:29 -0500

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,19 @@
+// pages/_app.js
 import '../public/styles/bootstrap.min.css';
 import MainNav from '@/components/MainNav';
 import { Container } from 'react-bootstrap';
+import RouteGuard from '@/components/RouteGuard'; // Import the new component
 
 export default function MyApp({ Component, pageProps }) {
   return (
     <>
       <MainNav />
-      <Container className="pb-5">
-        <Component {...pageProps} />
-      </Container>
+      {}
+      <RouteGuard>
+        <Container className="pb-5">
+          <Component {...pageProps} />
+        </Container>
+      </RouteGuard>
     </>
   );
 }
