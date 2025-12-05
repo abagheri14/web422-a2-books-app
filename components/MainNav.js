7dca31ecc9aebcd56a1d220fa27e4f8e4beea0ca
Ali Bagheri <155804715+abagheri14@users.noreply.github.com>
2025-12-05 15:58:16 -0500

    Update MainNav.js

--- components/MainNav.js
+++ components/MainNav.js
@@ -1,7 +1,16 @@
 import Link from 'next/link';
 import { Container, Nav, Navbar } from 'react-bootstrap';
+import { isAuthenticated, removeToken } from '@/lib/authenticate';
+import { useRouter } from 'next/router';
 
 export default function MainNav() {
+  const router = useRouter();
+
+  function handleLogout() {
+    removeToken();
+    router.push("/login");
+  }
+
   return (
     <Navbar bg="light" expand="lg" className="mb-4 border-bottom">
       <Container>
@@ -9,9 +18,19 @@ export default function MainNav() {
         <Navbar.Toggle aria-controls="main-nav" />
         <Navbar.Collapse id="main-nav">
           <Nav className="me-auto">
-            {/* remove "Books" link */}
             <Nav.Link as={Link} href="/favourites">Favourites</Nav.Link>
             <Nav.Link as={Link} href="/about">About</Nav.Link>
+          </Nav>
+          
+          <Nav>
+            {!isAuthenticated() ? (
+              <>
+                <Nav.Link as={Link} href="/register">Register</Nav.Link>
+                <Nav.Link as={Link} href="/login">Login</Nav.Link>
+              </>
+            ) : (
+              <Nav.Link onClick={handleLogout}>Logout</Nav.Link>
+            )}
           </Nav>
         </Navbar.Collapse>
       </Container>
